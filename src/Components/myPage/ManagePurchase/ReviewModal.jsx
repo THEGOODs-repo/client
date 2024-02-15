89741af258e61081bf5d12a8f71e0516d7b7c48f
juns-yang <rfgg5333@office.khu.ac.kr>
2024-02-16 01:20:05 +0900

    Feature : order // API연동 및 회원/비회원 분기

---
+++ src/Components/myPage/ManagePurchase/ReviewModal.jsx
@@ -0,0 +1,305 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(32, 33, 35, 0.5);
+  display: ${(e) => (e.$display ? "flex" : "none")};
+  align-items: center;
+  justify-content: center;
+`;
+
+const Modal = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  background: white;
+  padding: ${22 / 19.2}vw ${25 / 19.2}vw;
+  width: ${550 / 19.2}vw;
+  border-radius: ${5 / 19.2}vw;
+  background: #f9f9f9;
+
+  /* box */
+  box-shadow: 0px 0px ${10 / 19.2}vw ${1 / 19.2}vw rgba(0, 0, 0, 0.25);
+`;
+
+const ConfirmButton = styled.div`
+  display: flex;
+  width: ${160 / 19.2}vw;
+  height: ${44 / 19.2}vw;
+  flex-shrink: 0;
+  border-radius: ${5 / 19.2}vw;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+  font-size: ${18 / 19.2}vw;
+  padding: 0;
+  background: #f0c920;
+  margin: ${18 / 19.2}vw 0 0 0;
+  align-self: center;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 0 0 0 0;
+  padding: 0;
+  font-size: ${20 / 19.2}vw;
+  justify-content: center;
+  align-items: center;
+  color: #202123;
+  flex-direction: column;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 0 0 0 0;
+  padding: 0;
+  font-size: ${14 / 19.2}vw;
+  justify-content: center;
+  align-items: center;
+  color: #52555b;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const RatingWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 0 0 0 0;
+  padding: 0;
+  font-size: ${32 / 19.2}vw;
+  height: ${77 / 19.2}vw;
+
+  justify-content: center;
+  align-items: center;
+  color: #202123;
+  flex-direction: row;
+  background: rgba(156, 156, 156, 0.15);
+  border-radius: ${5 / 19.2}vw;
+`;
+
+const ItemWrapper = styled.div`
+  width: 100%;
+  padding: 0;
+  margin: ${20 / 19.2}vw 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const ItemImg = styled.img`
+  width: ${130 / 19.2}vw;
+  border-radius: ${5 / 19.2}vw;
+`;
+
+const ItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  line-height: 1.6;
+  margin: 0 0 0 ${13 / 19.2}vw;
+  font-size: ${13 / 19.2}vw;
+`;
+
+const ReviewWrapper = styled.div`
+  width: 100%;
+  height: ${200 / 19.2}vw;
+  border-radius: ${5 / 19.2}vw;
+  border: ${1 / 19.2}vw solid rgba(156, 156, 156, 0.5);
+  padding: ${14 / 19.2}vw;
+  font-size: ${14 / 19.2}vw;
+  box-sizing: border-box;
+`;
+
+const WarningWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin: ${10 / 19.2}vw 0 0 0;
+  padding: 0 0 0 ${16 / 19.2}vw;
+  font-size: ${12 / 19.2}vw;
+  height: ${57 / 19.2}vw;
+  line-height: 1.2;
+  justify-content: center;
+  align-items: flex-start;
+  color: #52555b;
+  flex-direction: column;
+  background: rgba(156, 156, 156, 0.15);
+  border-radius: ${5 / 19.2}vw;
+  box-sizing: border-box;
+`;
+
+const ReviewModal = ({
+  isOpen,
+  onClose,
+  ItemName,
+  OptionString,
+  OrderItemId,
+}) => {
+  const token = useSelector((state) => state.login.token);
+  const [Rating, SetRating] = useState(5);
+  const [Review, SetReview] = useState("");
+  const HandleReviewOrder = async () => {
+    try {
+      const endpoint = `/api/order/${OrderItemId}/complete`;
+
+      const response = await axios.post(endpoint, null, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.data.isSuccess === true) {
+        console.log(response);
+        onClose();
+      }
+    } catch (error) {
+      console.error("Error during POST request:", error);
+    }
+  };
+
+  return (
+    <ModalWrapper $display={isOpen}>
+      <Modal>
+        <TitleWrapper>후기 작성</TitleWrapper>
+        <TextWrapper>
+          받으신 결과물은 만족스러우셨나요?
+          <br />
+          서비스에 대한 만족도를 별점으로 남겨주세요.
+        </TextWrapper>
+        <RatingWrapper>
+          {Rating}
+          {".0 "}
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => SetRating(1)}
+          >
+            <g clipPath="url(#clip0_1653_178)">
+              <path
+                d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z"
+                fill={Rating > 0 ? "#FFC700" : "#D9D9D9"}
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1653_178">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => SetRating(2)}
+          >
+            <g clipPath="url(#clip0_1653_178)">
+              <path
+                d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z"
+                fill={Rating > 1 ? "#FFC700" : "#D9D9D9"}
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1653_178">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => SetRating(3)}
+          >
+            <g clipPath="url(#clip0_1653_178)">
+              <path
+                d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z"
+                fill={Rating > 2 ? "#FFC700" : "#D9D9D9"}
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1653_178">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => SetRating(4)}
+          >
+            <g clipPath="url(#clip0_1653_178)">
+              <path
+                d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z"
+                fill={Rating > 3 ? "#FFC700" : "#D9D9D9"}
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1653_178">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => SetRating(5)}
+          >
+            <g clipPath="url(#clip0_1653_178)">
+              <path
+                d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z"
+                fill={Rating > 4 ? "#FFC700" : "#D9D9D9"}
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1653_178">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        </RatingWrapper>
+        <ItemWrapper>
+          <ItemImg src="" alt="상품이미지" />
+          <ItemInfo>
+            <span style={{ fontSize: `${16 / 19.2}vw` }}>{ItemName}</span>
+            {OptionString}
+          </ItemInfo>
+        </ItemWrapper>
+        후기
+        <ReviewWrapper
+          onInput={(e) => SetReview(e.target.innerText)}
+          contentEditable
+          suppressContentEditableWarning
+        />
+        <WarningWrapper>
+          후기를 작성하면 자동 구매 확정이 됩니다.
+          <br />
+          구매 확정 이후에는 반품 신청이 불가합니다.
+        </WarningWrapper>
+        <ConfirmButton onClick={() => HandleReviewOrder()}>확인</ConfirmButton>
+      </Modal>
+    </ModalWrapper>
+  );
+};
+
+export default ReviewModal;
--- src/Components/myPage/myPage(Customer)/ManagePurchase.jsx
+++ src/Components/myPage/myPage(Customer)/ManagePurchase.jsx
@@ -1,53 +1,229 @@
-import React from "react";
-import { BeforePurchase } from "../ManagePurchase/BeforePurchase";
-import { CompletePurchase } from "../ManagePurchase/CompletePurchase";
-import { ReadyToDelivery } from "../ManagePurchase/ReadyToDelivery";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { StartDelivery } from "../ManagePurchase/StartDelivery";
-import { CompleteDelivery } from "../ManagePurchase/CompleteDelivery";
+import OrderItems from "../ManagePurchase/OrderItems";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import OrderDetail from "../ManagePurchase/OrderDetail";
 
+const OrderStatusEnum = {
+  PAY_PREV: "PAY_PREV",
+  PAY_COMP: "PAY_COMP",
+  DEL_PREP: "DEL_PREP",
+  DEL_START: "DEL_START",
+  DEL_COMP: "DEL_COMP",
+  CONFIRM: "CONFIRM",
+  CANCEL: "CANCEL",
+  REFUND_ONGOING: "REFUND_ONGOING",
+  REFUND_COMP: "REFUND_COMP",
+};
+
+export default function TopElement() {
+  return (
+    <Routes>
+      <Route path="/" element={<ManagePurchase />} />
+      <Route path=":OrderItemId" element={<OrderDetail />} />
+    </Routes>
+  );
+}
 //마이페이지_구매관리
-export default function ManagePurchase() {
-  const fileInput = React.useRef(null);
+export function ManagePurchase({
+  Guest,
+  OrderNumber,
+  OrderName,
+  OrderContact,
+}) {
+  const token = useSelector((state) => state.login.token);
+  const [OrderStatusFilter, SetOrderStatusFilter] = useState(null);
+  const [CurrentPage, SetCurrentPage] = useState(1);
+  const [OrderItemList, SetOrderItemList] = useState([]);
+  const [TotalPage, SetTotalPage] = useState(1);
+  Guest = Guest || false;
 
-  const handleButtonClick = (e) => {
-    fileInput.current.click();
+  const handlePageChange = (pageNumber) => {
+    SetCurrentPage(pageNumber);
   };
+
+  useEffect(() => {
+    console.log(Guest);
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const endpoint = `/api/order/?page=${CurrentPage}${OrderStatusFilter === null ? "" : `&status=${OrderStatusFilter}`}`;
+
+        const response = await axios.get(endpoint, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.data.isSuccess === true) {
+          Array.isArray(response.data.result.orderItemPreViewDTOList) &&
+            response.data.result.orderItemPreViewDTOList.length > 0 &&
+            SetOrderItemList(() => [
+              ...response.data.result.orderItemPreViewDTOList,
+            ]);
+          SetTotalPage(response.data.result.totalPage);
+        }
+      } catch (error) {
+        console.error("Error during POST request:", error);
+      }
+    };
+
+    const fetchGuest = async () => {
+      try {
+        const endpoint = `/api/order/api/nologin/order`;
+        const requestBody = {
+          ordersId: OrderNumber,
+          name: OrderName,
+          phone: OrderContact,
+          page: CurrentPage,
+        };
+
+        const response = await axios.post(endpoint, requestBody, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.data.isSuccess === true) {
+          Array.isArray(response.data.result.orderItemPreViewDTOList) &&
+            response.data.result.orderItemPreViewDTOList.length > 0 &&
+            SetOrderItemList(() => [
+              ...response.data.result.orderItemPreViewDTOList,
+            ]);
+          SetTotalPage(response.data.result.totalPage);
+        }
+      } catch (error) {
+        console.error("Error during POST request:", error);
+      }
+    };
+
+    !Guest && fetchData();
+    Guest && fetchGuest();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [CurrentPage, OrderStatusFilter]);
 
   return (
     <MainContainer>
-      <h1>구매관리</h1>
-      <ButtonContainer>
-        <Button style={{ width: 65, height: 37 }} onClick={handleButtonClick}>
-          전체
-        </Button>
-        <Button style={{ width: 73, height: 37 }} onClick={handleButtonClick}>
-          결제전
-        </Button>
-        <Button style={{ width: 86, height: 37 }} onClick={handleButtonClick}>
-          결제완료
-        </Button>
-        <Button style={{ width: 86, height: 37 }} onClick={handleButtonClick}>
-          배송준비
-        </Button>
-        <Button style={{ width: 86, height: 37 }} onClick={handleButtonClick}>
-          거래종료
-        </Button>
-        <Button style={{ width: 86, height: 37 }} onClick={handleButtonClick}>
-          주문취소
-        </Button>
-        <Button style={{ width: 110, height: 37 }} onClick={handleButtonClick}>
-          자동주문취소
-        </Button>
-        <Button style={{ width: 73, height: 37 }} onClick={handleButtonClick}>
-          삭제됨
-        </Button>
-      </ButtonContainer>
-      <BeforePurchase />
-      <CompletePurchase />
-      <ReadyToDelivery />
-      <StartDelivery />
-      <CompleteDelivery />
+      {!Guest ? <h1>구매관리</h1> : <h1>주문 상품</h1>}
+      {!Guest && (
+        <ButtonContainer>
+          <Button
+            style={{ width: 50, height: 37 }}
+            onClick={() => SetOrderStatusFilter(null)}
+            className={OrderStatusFilter === null && "selected"}
+          >
+            전체
+          </Button>
+          <Button
+            style={{ width: 73, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.PAY_PREV)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.PAY_PREV && "selected"
+            }
+          >
+            결제전
+          </Button>
+          <Button
+            style={{ width: 86, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.PAY_COMP)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.PAY_COMP && "selected"
+            }
+          >
+            결제완료
+          </Button>
+          <Button
+            style={{ width: 86, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.DEL_PREP)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.DEL_PREP && "selected"
+            }
+          >
+            배송준비
+          </Button>
+          <Button
+            style={{ width: 86, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.DEL_START)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.DEL_START && "selected"
+            }
+          >
+            배송시작
+          </Button>
+          <Button
+            style={{ width: 86, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.DEL_COMP)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.DEL_COMP && "selected"
+            }
+          >
+            배송완료
+          </Button>
+          <Button
+            style={{ width: 86, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.CONFIRM)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.CONFIRM && "selected"
+            }
+          >
+            구매확정
+          </Button>
+          <Button
+            style={{ width: 86, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.CANCEL)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.CANCEL && "selected"
+            }
+          >
+            주문취소
+          </Button>
+          <Button
+            style={{ width: 96, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.REFUND_ONGOING)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.REFUND_ONGOING && "selected"
+            }
+          >
+            반품진행중
+          </Button>
+          <Button
+            style={{ width: 73, height: 37 }}
+            onClick={() => SetOrderStatusFilter(OrderStatusEnum.REFUND_COMP)}
+            className={
+              OrderStatusFilter === OrderStatusEnum.REFUND_COMP && "selected"
+            }
+          >
+            반품완료
+          </Button>
+        </ButtonContainer>
+      )}
+      {Array.isArray(OrderItemList) && OrderItemList.length > 0
+        ? OrderItemList.map((data, index) => (
+            <OrderItems {...data} key={index} />
+          ))
+        : "주문하신 상품이 없습니다!"}
+      {!Guest && (
+        <PageNationWrapper>
+          <PageNation className="prevprev" />
+          <PageNation className="prev" />
+          {Array.from({
+            length: TotalPage,
+          }).map((_, index) => (
+            <PageNation
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              disabled={index + 1 === CurrentPage}
+            >
+              {index + 1}
+            </PageNation>
+          ))}
+          <PageNation className="next" />
+          <PageNation className="nextnext" />
+        </PageNationWrapper>
+      )}
     </MainContainer>
   );
 }
@@ -69,7 +245,6 @@ const MainContainer = styled.div`
     font-style: normal;
     font-weight: 700;
     font-size: 26px;
-    line-height: 35px;
     color: #202123;
   }
 `;
@@ -108,5 +283,58 @@ const Button = styled.button`
     text-align: center;
 
     color: #202123;
+  }
+
+  &.selected {
+    border: 1.5px solid;
+    border-color: black;
+
+    font-family: "Noto Sans";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 20px;
+    text-align: center;
+
+    color: #202123;
+  }
+`;
+
+const PageNationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${14 / 19.2}vw;
+`;
+
+const PageNation = styled.div`
+  width: ${32 / 19.2}vw;
+  height: ${32 / 19.2}vw;
+  border: ${1 / 19.2}vw solid rgba(156, 156, 156);
+  border-radius: ${1 / 19.2}vw;
+  color: ${(props) =>
+    props.$selected ? "rgba(32,33,35)" : "rgba(156,156,156)"};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &.prevprev {
+    background: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTUiIHZpZXdCb3g9IjAgMCAxOCAxNSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTguMjUgMTEuODc1TDMgNy41TDguMjUgMy4xMjVNMTQuMjUgMTEuODc1TDkgNy41TDE0LjI1IDMuMTI1IiBzdHJva2U9IiM5QzlDOUMiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgo8L3N2Zz4K")
+      no-repeat center center;
+  }
+
+  &.prev {
+    background: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTUiIHZpZXdCb3g9IjAgMCAxOCAxNSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTExLjI1IDExLjg3NUw2IDcuNUwxMS4yNSAzLjEyNSIgc3Ryb2tlPSIjOUM5QzlDIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==")
+      no-repeat center center;
+  }
+
+  &.next {
+    background: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTUiIHZpZXdCb3g9IjAgMCAxOCAxNSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTYuNzUgMy4xMjVMMTIgNy41TDYuNzUgMTEuODc1IiBzdHJva2U9IiM5QzlDOUMiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgo8L3N2Zz4K")
+      no-repeat center center;
+  }
+
+  &.nextnext {
+    background: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTUiIHZpZXdCb3g9IjAgMCAxOCAxNSIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTkuNzUgMy4xMjVMMTUgNy41TDkuNzUgMTEuODc1TTMuNzUgMy4xMjVMOSA3LjVMMy43NSAxMS44NzUiIHN0cm9rZT0iIzlDOUM5QyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+Cjwvc3ZnPgo=")
+      no-repeat center center;
   }
 `;
